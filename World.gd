a982e811798597c4eae73e3f9f415c43ab0e3808
Michael Lesniak <mail@mlesniak.com>
2020-01-21 16:37:44 +0100

    Add target image

--- World.gd
+++ World.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
+signal catchable_player
+
 func _ready():
 	$music.play()
 	$Score_1.text = "0"
 	$Score_2.text = "0"
 	
+	start()
+	
+func start():
+	emit_signal("catchable_player", $Player_1)
 	$Player_1.catchable = true
 	$Player_1.position.x = 0
 	$Player_1.position.y = get_viewport().size.y / 2 
@@ -13,7 +19,7 @@ func _ready():
 	$Player_2.position.x = get_viewport().size.x
 	$Player_2.position.y = get_viewport().size.y / 2 
 	
-	$Timer.start()
+	$Timer.start()	
 
 func _process(delta):
 	if Input.is_action_just_released("exit"):
@@ -30,3 +36,12 @@ func _on_Player_hit(source, target):
 	$Player_2.catchable = !$Player_2.catchable
 	$Player_2.position.x = get_viewport().size.x
 	$Player_2.position.y = get_viewport().size.y / 2 
+	
+func switch_side():
+	$Player_1.catchable = !$Player_1.catchable	
+	$Player_2.catchable = !$Player_2.catchable
+	
+	if $Player_1.catchable:
+		emit_signal("catchable_player", $Player_1)
+	else:
+		emit_signal("catchable_player", $Player_2)
