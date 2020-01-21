8916a89e1c10443883b9eaa24ad00e2eed9ec2be
Michael Lesniak <mail@mlesniak.com>
2020-01-21 14:35:50 +0100

    Add background

--- Player.gd
+++ Player.gd
@@ -1,6 +1,25 @@
 extends KinematicBody2D
 
 export var texture: Texture
+export var gamepad: int = 0
+export var speed: int = 50
 
 func _ready():
 	$Sprite.texture = texture
+
+func get_force(direction):
+	var name = str(gamepad) + "_move_" + direction
+	return Input.get_action_strength(name)
+
+func compute_velocity():
+	var dx =  get_force("right") - get_force("left")
+	var dy =  get_force("down") - get_force("up")
+	var velocity = Vector2(dx, dy)
+	velocity = velocity.normalized() * speed
+	return velocity
+
+func _process(delta):
+	var velocity = compute_velocity()
+	move_and_collide(velocity)
+	
+	
--- World.gd
+++ World.gd
@@ -4,3 +4,6 @@ extends Node2D
 func _process(delta):
 	if Input.is_action_just_released("exit"):
 		get_tree().quit()
+		
+	if Input.is_action_just_released('fullscreen'):
+		OS.window_fullscreen = !OS.window_fullscreen	
